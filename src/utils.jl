b846aaddd223a6b07c5b5903eed9b5f03f39460e
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-10-21 16:19:07 +0200

    modify indexing order

--- src/utils.jl
+++ src/utils.jl
@@ -2,7 +2,7 @@ export sort_tile_indices
 
 function sort_tile_indices(xs, nx, ys, ny)
     ntiles = nx*ny
-    yx = [(ys[n], xs[n]) for n in 1:ntiles]
-    sortedinds = sortperm(yx, by = a -> (-a[1], -a[2]))
+    xy = [(xs[n], ys[n]) for n in 1:ntiles]
+    sortedinds = sortperm(xy, by = a -> (-a[1], a[2]))
     return reshape(sortedinds, (ny, nx))
 end # function
